4ddc58902006f7a004f2d3e5762c282781e0487c
killerarz <milov52@gmail.com>
2023-06-12 00:16:52 +0300

    Fixed Connection error

--- reader.py
+++ reader.py
@@ -13,16 +13,19 @@ async def read_chat(params):
     reader, _ = await asyncio.open_connection(params.server, params.port)
 
     while True:
-        chat_data = await reader.readline()
-        if not chat_data:
-            break
+        try:
+            chat_data = await reader.readuntil()
 
-        now = datetime.now()
-        chat_data = now.strftime(DATE_TIME_FORMAT) + chat_data.decode()
-        async with aiofiles.open(params.file_path, "a") as myfile:
-            await myfile.write(chat_data)
-        print(chat_data.strip())
+            if not chat_data:
+                break
 
+            now = datetime.now()
+            chat_data = now.strftime(DATE_TIME_FORMAT) + chat_data.decode()
+            async with aiofiles.open(params.file_path, "a") as myfile:
+                await myfile.write(chat_data)
+            print(chat_data.strip())
+        except asyncio.TimeoutError:
+            print("Превышено время ожидания")
 
 if __name__ == '__main__':
     arg_parser = configure_reader_argument_parser()
